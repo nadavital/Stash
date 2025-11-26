bcc91fb9f69967081377640d01e517cdc2613ef1
Nadav Avital <nadav.avital@icloud.com>
2025-11-26 12:44:38 -0800

    Initial Commit

---
+++ Stash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stash
+//
+//  Created by Avital, Nadav on 11/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stash/StashApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashApp.swift
+//  Stash
+//
+//  Created by Avital, Nadav on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct StashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
